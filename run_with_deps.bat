bbff7e146f3c42eb0f8e7373fc9810bc9dd846ed
Divyanshu K <divyanshu3388mth@gmail.com>
2025-08-04 17:34:00 +0530

    Delete run_with_deps.bat

--- run_with_deps.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing dependencies...
-py -3.11 -m pip install flask flask-sqlalchemy werkzeug authlib requests waitress
-
-echo Starting Fashion Store E-commerce Website...
-echo.
-py -3.11 app.py
-pause 
